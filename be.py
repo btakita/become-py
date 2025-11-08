adc3d028e76ace2ebe0de455a26405a4a1e48210
Brian Takita <brian.takita@gmail.com>
2025-11-08 01:03:29 -0500

    Api is be and be_singleton which can be subclassed.

--- be.py
+++ be.py
@@ -1,31 +1,11 @@
-
 from typing import Callable, Generic, Optional, TypeVar
 
-__all__ = ["Be"]
+__all__ = ["be", "be_singleton"]
 
 T = TypeVar("T")
 
-class BeMeta(type):
-    def __call__(cls, *args, **kwargs):
-        # If called with a dict argument, treat as class method call
-        if len(args) == 1 and isinstance(args[0], dict) and not kwargs:
-            # Create a temporary instance and call it
-            instance = super().__call__()
-            if hasattr(instance, 'callable'):
-                return instance(args[0])
-            else:
-                raise NotImplementedError("Class must implement callable method")
-        # Otherwise, normal constructor call
-        return super().__call__(*args, **kwargs)
-
-class Be(Generic[T], metaclass=BeMeta):
+class BeBase(Generic[T]):
     callable: Callable[[dict], T]
-
-    @classmethod
-    def new(cls, callable: Callable[[dict], T]) -> "Be":
-        be = Be[T]()
-        be.callable = callable
-        return be
 
     def __call__(self, ctx: dict) -> T:
         if self in ctx:
@@ -39,3 +19,16 @@ class Be(Generic[T], metaclass=BeMeta):
 
     def is_in(self, ctx: dict) -> bool:
         return self in ctx
+
+class be_singleton(BeBase[T]):
+    instance: Optional[BeBase[T]] = None
+
+    def __new__(cls, ctx: dict) -> T:
+        if cls.instance is None:
+            cls.instance = super().__new__(cls)
+
+        return cls.instance(ctx)
+
+class be(BeBase[T]):
+    def __init__(self, callable: Callable[[dict], T]) -> None:
+        self.callable = callable
--- be_test.py
+++ be_test.py
@@ -1,17 +1,17 @@
-from be import Be
+from be import be, be_singleton
 
 
 def be_foo(ctx: dict) -> int:
     return 1
 
-be_foo = Be.new(be_foo)
+be_foo = be(be_foo)
 
-class be_bar(Be[int]):
+class be_bar(be_singleton[int]):
     def callable(self, ctx: dict) -> int:
         return be_foo(ctx) + 100
 
 if __name__ == "__main__":
     ctx = dict()
     assert be_foo(ctx) == 1
-    assert be_bar(ctx) == 101
+    assert be_bar(ctx) == 101, (be_bar(ctx), 101)
     print("Success!")
